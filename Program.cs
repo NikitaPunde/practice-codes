b960b76bd2cb90f48d0fb4b0e9961f47b5b14fda
NikitaPunde <105191404+NikitaPunde@users.noreply.github.com>
2023-04-28 10:28:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace test_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
